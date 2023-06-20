6b07accaf4af904fbcb4a0435a39c92108ce8dfc
nils-matthaei <nils.matthaei@gmail.com>
2023-06-20 14:54:06 +0200

    fixed calculation of offset

--- src/lib/prettyprinter.rs
+++ src/lib/prettyprinter.rs
@@ -2,7 +2,7 @@ use super::transition::*;
 
 pub fn pretty_printer(_turing_machine: Vec<Transition>) {
     let mut turing_machine = _turing_machine;
-    let offset: usize = (turing_machine.len() % 3) + 1;
+    let offset: usize = 3 - turing_machine.len() % 3;
     for _ in 0..offset {
         turing_machine.push(build_transition(1, 4, 1));
     }
